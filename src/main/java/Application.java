54c594112fb269c1334da19bf8ce00c8618617c9
filip.drzewiecki <filip.drzewiecki@jit.team>
2023-01-25 22:21:58 +0100

    Test commit

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,7 +1,9 @@
-public class Apllication {
+public class Application {
 
     public static void main(String[] args) {
         System.out.println("Hello");
+
+        System.out.println("Tomato");
 
     }
 
